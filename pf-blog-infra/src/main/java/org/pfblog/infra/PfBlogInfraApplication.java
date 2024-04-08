449faf62b80c804dd34686755c682c9d48de6bb5
pf.zhang <pfz7z7@126.com>
2024-04-09 01:19:09 +0800

    初始化博客项目

---
+++ pf-blog-infra/src/main/java/org/pfblog/infra/PfBlogInfraApplication.java
@@ -0,0 +1,13 @@
+package org.pfblog.infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfBlogInfraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfBlogInfraApplication.class, args);
+    }
+
+}
---
+++ pf-blog-web/src/main/java/org/pfblog/web/PfBlogWebApplication.java
@@ -0,0 +1,13 @@
+package org.pfblog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfBlogWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfBlogWebApplication.class, args);
+    }
+
+}
